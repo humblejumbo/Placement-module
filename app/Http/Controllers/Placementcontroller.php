2ee7205de3eab4bb67bb2e92508289a1d15e8c5d
humblejumbo <goelshubham46@gmail.com>
2018-07-27 17:15:43 +0530

    placement table added

---
+++ app/Http/Controllers/Placementcontroller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Placementcontroller extends Controller
+{
+    //
+}
---
+++ database/migrations/2018_07_27_095221_create_placements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlacementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('placements', function (Blueprint $table) {
+           
+            $table->integer('stu_id');
+            $table->integer('comp_id');
+            $table->integer('year');
+            $table->boolean('is_selected');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('placements');
+    }
+}
